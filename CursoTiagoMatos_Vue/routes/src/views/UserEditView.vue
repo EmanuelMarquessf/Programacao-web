0925724d63897773db40b1e6a85421319ce0175d
Emanuel Fonseca <emanuel.marquessf@gmail.com>
2023-10-17 19:43:11 -0300

    study rotes and sass

---
+++ CursoTiagoMatos_Vue/routes/src/views/UserEditView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>id
+      {{ $route.query.page }}
+      <button @click="login()">
+        Login
+      </button>
+      <p></p>
+    </h1>
+  </div>
+</template>
+
+<script>
+  export default {
+  methods: {
+    login() {
+      this.$router.push('/about');
+      console.log('logou');
+    },
+  },
+};
+</script>
